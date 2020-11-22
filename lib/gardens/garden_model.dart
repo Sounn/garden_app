5112a8d226a584fb0348eaf1c2d4376e5b1a50fa
Sounn <kikuitisounn@gmail.com>
2020-11-22 19:02:48 +0900

    fix garden query

--- lib/gardens/garden_model.dart
+++ lib/gardens/garden_model.dart
@@ -1,14 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'garden.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 class GardenModel extends ChangeNotifier {
   List<Garden> gardens = [];
   String todoText = ''; 
+  final user = FirebaseAuth.instance.currentUser;
 
   Future fetchGardens() async {
     // ignore: deprecated_member_use
-    final docs = await FirebaseFirestore.instance.collection('gardens').getDocuments();
+    final docs = await FirebaseFirestore.instance.collection('gardens').where('uid', isEqualTo: user.uid).getDocuments();
     // ignore: deprecated_member_use
     final gardens = docs.documents.map((doc) => Garden(doc)).toList();
     this.gardens = gardens;
