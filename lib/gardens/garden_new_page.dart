e7ad1d23e1349a7e33b7c3e07117dd3cddfae8da
Sounn <kikuitisounn@gmail.com>
2020-12-08 17:44:15 +0900

    add garden_new image , fix info.plist

--- lib/gardens/garden_new_page.dart
+++ lib/gardens/garden_new_page.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 class GardenNewPage extends StatefulWidget {
   @override
@@ -71,6 +73,33 @@ class _ImageUploadPage extends StatefulWidget{
 }
 
 class __ImageUploadPageState extends State<_ImageUploadPage> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future chooseImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -81,32 +110,60 @@ class __ImageUploadPageState extends State<_ImageUploadPage> {
               color: Colors.green,//change your color here
           ),
         ),
-        body:Container(
-          child:Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            // mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Container(
-                child: Text("写真",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                        color: Colors.black,
-                        ),
-                      )
-              ),
-              Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 50.0),
-                  child: ElevatedButton(
-                    child: Text('次へ'),
-                    onPressed: () async {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => _ImageUploadPage()));
-                    },
+        body:Center(
+              child: _image == null
+                  ? Text('画像が選択されていません。')
+                  : Image.file(_image),
+            ),
+            floatingActionButton: Column(
+              verticalDirection: VerticalDirection.up, // childrenの先頭を下に配置
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                FloatingActionButton(
+                  heroTag: "hero1",
+                  onPressed: getImage,
+                  tooltip: 'Pick Image',
+                  child: Icon(Icons.add_a_photo)
+                  // backgroundColor: Colors.redAccent,
+                ),
+                Container( // 余白のためContainerでラップ
+                  margin: EdgeInsets.only(bottom: 16.0), 
+                  child: FloatingActionButton(
+                    heroTag: "hero2",
+                    onPressed: chooseImage,
+                    tooltip: 'Pick Image',
+                    child: Icon(Icons.photo_album),
+                    // backgroundColor: Colors.amberAccent,
                   ),
                 ),
-            ],
-          ),
-        )
+              ], 
+            )
     );
   }
 }
+// Container(
+//           child:Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             // mainAxisSize: MainAxisSize.min,
+//             children: <Widget>[
+//               Container(
+//                 child: Text("写真",
+//                       style: TextStyle(
+//                         fontWeight: FontWeight.bold,
+//                         fontSize: 25,
+//                         color: Colors.black,
+//                         ),
+//                       )
+//               ),
+//               Padding(
+//                   padding: const EdgeInsets.symmetric(vertical: 50.0),
+//                   child: ElevatedButton(
+//                     child: Text('次へ'),
+//                     onPressed: () async {
+//                       Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => _ImageUploadPage()));
+//                     },
+//                   ),
+//                 ),
+//             ],
+//           ),
+//         )
