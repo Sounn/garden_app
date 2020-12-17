f554956111d323d50b36444a9230e00d41a72d07
Sounn <kikuitisounn@gmail.com>
2020-12-17 10:25:51 +0900

    remove package:path_provider

--- lib/gardens/garden_new_page.dart
+++ lib/gardens/garden_new_page.dart
@@ -4,7 +4,6 @@ import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:path_provider/path_provider.dart';
 
 class GardenNewPage extends StatefulWidget {
   @override
