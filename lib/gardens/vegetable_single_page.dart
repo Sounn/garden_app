8c306aa06b603a97522bf028ff8547d54e581639
Sounn <kikuitisounn@gmail.com>
2020-12-19 09:37:05 +0900

    fix vegetable_single_page

--- lib/gardens/vegetable_single_page.dart
+++ lib/gardens/vegetable_single_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 class VegetableSinglePage extends StatefulWidget {
   @override
   _VegetableSinglePageState createState() => _VegetableSinglePageState();
@@ -15,17 +16,43 @@ class _VegetableSinglePageState extends State<VegetableSinglePage> {
         return Scaffold(
           appBar: AppBar(
             title: Text(snapshot.data["vegetable"]),
+            centerTitle: true,
             backgroundColor: Colors.green,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: (){
+                  //編集ページに
+                  print("編集する");
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.restore_from_trash),
+                onPressed: (){
+                  //モーダルで消去するかを確認
+                  print("消去する");
+                },
+              ),
+            ],
           ),
           body:Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Container(
                 child: Image.network(snapshot.data["imageURL"]),
               ),
               Container(
-                child:Text(snapshot.data["vegetable"]),
+                child: Text('次の水やり　：　')
+              ),
+              Container(
+                child: Text('植えてから何日目　：　')
+              ),
+              Container(
+                child: Text('植えた日' + DateFormat('yyyy-MM-dd')
+                            .format(snapshot.data["createdAt"].toDate())
+                            .toString()
+                            ),
               ),
             ]
           ),
